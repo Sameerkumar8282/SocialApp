5fe25afbc01718e56cd424b4b9ff470e831c48c2
Sameer Kumar <sameerkumar1012.sk@gmail.com>
2024-08-27 10:48:08 +0530

    First Commit

---
+++ Backend/SocialApp/src/test/java/com/MySocial/SocialApp/SocialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MySocial.SocialApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
